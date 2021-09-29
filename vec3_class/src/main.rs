dd93baf141a183fe1d2d8a9ef4bd5771171d3cca
qinyuhao <gardenseed01@gmail.com>
2021-09-29 22:54:11 +0800

    vec3h dev over

--- vec3_class/src/main.rs
+++ vec3_class/src/main.rs
@@ -9,11 +9,21 @@ impl Vec3H {
     fn x(&self) -> f64 {
         self.values[0]
     }
+
     fn y(&self) -> f64 {
         self.values[1]
     }
+
     fn z(&self) -> f64 {
         self.values[2]
+    }
+
+    fn length_squared(&self) -> f64 {
+        self[0] * self[0] + self[1] * self[1] + self[2] * self[2]  
+    }
+
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
     }
 }
 
@@ -81,7 +91,7 @@ impl std::ops::MulAssign<f64> for Vec3H {
 
 impl std::ops::Div<f64> for Vec3H {
     type Output = Vec3H;
-    fn div(self, _rsh) -> Self::Output {
+    fn div(self, _rsh: f64) -> Self::Output {
         Vec3H {
             values:vec![
                 self[0] / _rsh,
@@ -92,6 +102,25 @@ impl std::ops::Div<f64> for Vec3H {
     }
 }
 
-fn main() {
-    
+impl std::ops::DivAssign<f64> for Vec3H {
+    fn div_assign(&mut self, _rsh: f64) {
+        self.values = self.values.iter().map(|&x| x /  _rsh).collect::<Vec<f64>>()
+    }
 }
+
+impl std::fmt::Display for Vec3H {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Vec3H -> x:{} y:{} z:{}", self[0], self[1], self[2])
+    }
+}
+
+type Color = Vec3H;
+type Point3 = Vec3H;
+
+fn main() {
+    let a = Color {
+        values:vec![0f64,0.1,0.1]
+    };
+    println!("{}  haha \n{}", a, a)
+}
+
